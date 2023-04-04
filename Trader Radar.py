6dd76df2854dfd728b6b722ccede929bf4bd01c5
Nichoolaas11 <100955527+Nichoolaas11@users.noreply.github.com>
2023-04-04 18:45:47 -0400

    Add files via upload

---
+++ Trader Radar.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+
+# Define the function to get S&P 500 tickers
+def get_sp500_tickers():
+    table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    df = table[0]
+    return df.Symbol.tolist()
+
+# Define the function to get stock data and calculate the standard deviation
+def get_data(ticker):
+    stock_data = yf.download(ticker)
+    volume_std = stock_data['Volume'].std()
+    return stock_data, volume_std
+
+# Define the Streamlit app layout
+st.write("""
+# S&P 500 Trading Volume Alert App
+""")
+
+# Get the S&P 500 tickers
+tickers = get_sp500_tickers()
+
+# Loop through each ticker and check if the volume deviates from the standard deviation
+alert_tickers = []
+for ticker in tickers:
+    try:
+        stock_data, volume_std = get_data(ticker)
+        last_volume = stock_data['Volume'][-1]
+        if last_volume > volume_std:
+            alert_tickers.append(ticker)
+    except:
+        pass
+
+# Display the list of alert tickers
+st.write(f"### Alert: {len(alert_tickers)} stocks deviate from the standard deviation!")
+st.write(alert_tickers)
+
+#Closing Message
+
+st.write('This list of tickers represents unusual volume activity. This can mean many things and is only an indiciation of heavy movement in the market.')
---
+++ stocks.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import plotly.express as px
+from stocknews import StockNews
+
+
+
+
+st.title('Stock Display')
+st.text('This web app displays stock prices from companies in the S&P 500')
+st.text('Select a ticker and any time frame desired~')
+st.markdown('Additionally, the **Trader Radar** will display tickers with unusual volume deviations from the mean')
+
+#variable declaration
+ticker = st.text_input('Ticker')
+startDate = st.date_input('Start Date')
+endDate = st.date_input('End Date')
+
+#Daily Percent Change
+#TRY TO ADD!!!!!
+
+
+if ticker:
+
+    #Graph
+    data = yf.download(ticker,start=startDate, end=endDate)
+    fig = px.line(data, x = data.index, y = data['Adj Close'], title = ticker)
+    st.plotly_chart(fig)
+
+
+    #Pricing Data, Fundamental Data, News
+
+    pricingData, fundamentalData, news = st.tabs(["Pricing Data", "Fundamental Data", "Relevant News"])
+
+    with pricingData:
+        st.write('Pricing Information')
+        daily=data
+        daily['% Change'] = data['Adj Close'] / data['Adj Close'].shift(1)-1
+        annualReturn = daily['% Change'].mean()*252*100
+        st.write('Annual Return:' , annualReturn, '%')
+        st.write(daily)
+    with fundamentalData:
+        st.write('Fundamental')
+
+    with news:
+        sn = StockNews(ticker, save_news=False)
+        df_news = sn.read_rss()
+        for i in range (7):
+       
+            st.subheader(df_news['title'][i])
+            article_name = df_news['title'][i].replace(" ", "-").lower() + ".html" # replace spaces with hyphens and convert to lowercase
+            st.write(f"[Link to article]({'https://finance.yahoo.com/m/' + df_news['guid'][i] + article_name})") # add hyperlink to title
+            #st.write(f"[Link to article]({df_news['guid'][i] + article_name})")
+            st.write(df_news['summary'][i])
+            title_sentiment = df_news['sentiment_title'][i]
+            st.write(f'Title Sentiment {title_sentiment}')
+            news_sentiment = df_news['sentiment_summary'][i]
+            st.write(f'News Sentiment {news_sentiment}')
+
+
